f69348189cf24962be3818a4df561afe22d3cdaf
linyisheng <linyisheng@tuniu.com>
2018-06-08 21:27:47 +0800

    init

---
+++ lua/com/yisheng/Helloworld.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: tn-ma-l0000207
+-- Date: 2018/6/8
+-- Time: 下午8:59
+-- To change this template use File | Settings | File Templates.
+--
+
+print 'helloworld'
